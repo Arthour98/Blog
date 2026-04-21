930bed6dc9a88600358ad69fa7a16a4a7a1bfdd1
artour <artourbouloudis@gmail.com>
2026-04-21 18:15:00 +0300

    refix 3 config

--- database/config.php
+++ database/config.php
@@ -1,33 +1,40 @@
-<?php class Database
+<?php
+
+class Database
 {
-    private $username = null;
-    private $password = null;
-    private $host = null;
-    private $dbname = null;
+    private $username;
+    private $password;
+    private $host;
+    private $dbname = "php_project2";
 
     private $connection;
 
     public function __construct()
     {
-        $env = parse_ini_file(__DIR__ . '/../.env');
-
-        $this->username = $env["DB_USERNAME"];
-        $this->password = $env["DB_PASSWORD"];
-        $this->host     = $env["DB_HOST"];
+        $this->username = getenv("DB_USERNAME");
+        $this->password = getenv("DB_PASSWORD");
+        $this->host     = getenv("DB_HOST");
     }
+
     public function connect()
     {
         $this->connection = null;
 
-
         try {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
 
-            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname;
-            $this->connection = new PDO($dsn, $this->username, $this->password);
+            $this->connection = new PDO(
+                $dsn,
+                $this->username,
+                $this->password
+            );
+
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         } catch (PDOException $e) {
-            die("ERROR:" . $e->getMessage());
+            die("DB ERROR: " . $e->getMessage());
         }
+
         return $this->connection;
     }
-}
+}?>
